08a1033da482273f41ad8d0c21c78b5601333930
Vivekachooz <vivekachooz@gmail.com>
2020-07-12 17:40:19 +0530

    accents: Add more accents
    The newly added accents are
    * BloodRed
    * IceBlue
    * Khaki
    * MidnightBrown
    * OrangeRed
    * WindowsBlue
    
    Signed-off-by: Vivekachooz <vivekachooz@gmail.com>

---
+++ themes/accents/BloodRed/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := BloodRed
+LOCAL_RRO_THEME := BloodRed
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_RRO_PACKAGE)
